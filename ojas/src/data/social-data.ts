aa708d9723a9db7ed635cac5a66da12b5c076ea6
Alice <ziqichu8@gmail.com>
2025-03-09 13:49:30 -0400

    social links

--- ojas/src/data/social-data.ts
+++ ojas/src/data/social-data.ts
@@ -1,5 +1,5 @@
 export const socialData = {
-  facebook: "https://www.facebook.com/share/1U7CqbrDs5/?mibextid=LQQJ4d",
+  facebook: "https://www.facebook.com/share/14oZD33oX3/?mibextid=LQQJ4d",
   instagram:
     "https://www.instagram.com/spa_ojas_winnipeg?igsh=NHBxcTV5eDkxb21i&utm_source=qr",
 };

d8654f17037540da502aa70ec2b416ef64e946fa
JC-Fresha <john.chang@fresha.com>
2025-09-04 16:16:08 -0700

    Updated booking link in link-data.ts

--- ojas/src/data/link-data.ts
+++ ojas/src/data/link-data.ts
@@ -1,5 +1,5 @@
 export const linkData = {
-    booking_link: 'https://www.fresha.com/a/h-plus-day-spa-winnipeg-60-saint-annes-road-dyyes1oi/booking?menu=true&dppub=true&rwg_token=AAh05qbNLDhHUwYFsPFQJT35KAUkVOI4pGROqRF0VsIazKyc08glP0ulxQeF_IbL60JKc_x63GmAepY9ue9fVqj0EGZc2jQnXQ%3D%3D&gei=g2XaZfLwOJW1ptQP2p6Z8Ag',
+    booking_link: 'https://www.fresha.com/book-now/spa-ojas-j3x7kgre/all-offer?share=true&pId=996404',
     gift_card_link: 'https://squareup.com/gift/ML6C7WAMHY4SG/order',
     domain_link: ''
 }
